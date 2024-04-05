fd7e4da4794deab50af5753c5652f4922dde4f3e
anbak98 <anbak98@gmail.com>
2024-04-05 13:50:38 +0900

    tow

--- UnityProject/Assets/01. Scripts/00. Managers/GameManager.cs
+++ UnityProject/Assets/01. Scripts/00. Managers/GameManager.cs
@@ -8,8 +8,10 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     private InputManager _input;
+    private StoneManager _stone;
 
     public static InputManager Input { get { return Instance._input;  } }
+    public static StoneManager Stone { get { return Instance._stone; } }
 
     void Awake()
     {
@@ -21,6 +23,11 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        _stone.OnStart();   
     }
 
     void Update()
