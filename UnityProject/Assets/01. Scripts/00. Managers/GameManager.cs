07e2e66d134300ea730715e7fdb4228591a86d72
anbak98 <anbak98@gmail.com>
2024-04-05 12:31:16 +0900

    Init

---
+++ UnityProject/Assets/01. Scripts/00. Managers/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private InputManager _input;
+
+    public static InputManager Input { get { return Instance._input;  } }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); Instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+}
